16406c44f80b1110779cbecfaba2ab492af583d5
paulakedouk <paulakedouk@gmail.com>
2018-02-26 13:47:53 -0800

    Adjust get reservations and show on the route

--- server.js
+++ server.js
@@ -11,58 +11,36 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var reservations = [];
+var reservations = [
+  {
+    routeName: 'yoda',
+    name: 'Yoda',
+    phone: '1',
+    email: 'blah@blah.com',
+    id: '123'
+  }
+];
 var waitList = [];
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'home.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'home.html'));
+// });
 
-app.get('/view', function(req, res) {
-  res.sendFile(path.join(__dirname, 'view.html'));
-});
+// app.get('/view', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'view.html'));
+// });
 
-app.get('/add', function(req, res) {
-  res.sendFile(path.join(__dirname, 'add.html'));
-});
+// app.get('/add', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'add.html'));
+// });
 
 // See all table reservations
-app.get('/api/:reservations?', function(req, res) {
-  var table = req.params.reservations;
-
-  if (table.length < 5) {
-    console.log(table);
-    for (var i = 0; i < reservations.length; i++) {
-      if (table === reservations[i].routeName) {
-        return res.json(reservations[i]);
-      }
-    }
-
-    return res.json(false);
-  }
-  return res.json(reservations);
-});
-
-// See all wait list
-app.get('/api/:waitList?', function(req, res) {
-  var waitTable = req.params.waitList;
-
-  if (waitTable) {
-    console.log(waitTable);
-
-    for (var i = 0; i < waitList.length; i++) {
-      if (waitTable === waitList[i].routeName) {
-        return res.json(waitList[i]);
-      }
-    }
-
-    return res.json(false);
-  }
-  return res.json(waitList);
+app.get('/api/reservations', function(req, res) {
+  res.json(reservations);
 });
 
 app.post('/api/add', function(req, res) {
